89c41727ae02e577670359ccdfa87aca11962253
signocancer <138729827+signocancer@users.noreply.github.com>
2023-07-05 18:06:43 -0500

    sannafood
    laricavida

---
+++ productos.js
@@ -0,0 +1,84 @@
+const productos = [
+  {
+    id: 1,
+    nombre: "Plato",
+    precio: 50,
+    img:
+    "comprar1.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 2,
+    nombre: "Plato",
+    precio: 100,
+    img:
+      "comprar1.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 3,
+    nombre: "Plato",
+    precio: 150,
+    img:
+      "comprar1.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 4,
+    nombre: "Plato",
+    precio: 200,
+    img:
+      "comprar1.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 5,
+    nombre: "Plato",
+    precio: 50,
+    img: "comprar1.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 6,
+    nombre: "Plato",
+    precio: 100,
+    img:
+      "comprar1.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 7,
+    nombre: "Plato",
+    precio: 150,
+    img:
+      "comprar1.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 8,
+    nombre: "Plato",
+    precio: 200,
+    img:
+      "comprar1.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 8,
+    nombre: "Plato",
+    precio: 200,
+    img:
+      "comprar1.jpg",
+    cantidad: 1,
+  },
+  {
+    id: 8,
+    nombre: "Plato",
+    precio: 200,
+    img:
+      "comprar1.jpg",
+    cantidad: 1,
+  },
+ 
+];
+  
+  
